4ee4222081cdb253a55b4c9287d114c403400fd8
Thomas Nichols <ta.nichols@outlook.com>
2022-12-18 13:14:30 -0800

    finalize chrome first pass

--- src/components/layout/TopBar.vue
+++ src/components/layout/TopBar.vue
@@ -1,22 +1,62 @@
 <template>
-    <div class="topbar p-3 surface-300 flex">
-        <router-link to="/" class="topbar-logo">
-            <img src="/images/logo.svg" alt="Grocery Tracker">
+    <div class="flex max-h-4rem shadow-4 p-2">
+        <router-link to="/" class="flex-none">
+            <img src="/images/logo.svg" class="h-full" alt="Grocery Tracker">
         </router-link>
-
-        <div class="layout-topbar-menu">
-            <Button icon="pi pi-check" class="p-button-rounded p-button-text" />
-            <LoginButton />
-        </div>
+        <div class="flex-grow-1" />
+        <Avatar
+            v-if="isAuthenticated"
+            :image="user.picture"
+            class="avatar mr-2 flex-none"
+            shape="circle"
+            size="large"
+            aria-haspopup="true"
+            aria-controls="profileMenu"
+            @click="toggleProfileMenu"
+        />
+        <Menu id="profileMenu" ref="profileMenu" :model="profileMenuItems" :popup="true" />
+        <Button
+            v-if="!isAuthenticated"
+            class="p-button-text flex-none"
+            icon="fa-solid fa-right-to-bracket"
+            label="Login"
+            @click="login"
+        />
     </div>
 </template>
 
 <script>
-import LoginButton from './LoginButton.vue'
-
 export default {
-    components: {
-        LoginButton
+    data: function () {
+        return {
+            isAuthenticated: this.$auth0.isAuthenticated,
+            user: this.$auth0.user,
+            profileMenuItems: [
+                {
+                    label: 'Profile',
+                    icon: 'fa-solid fa-user',
+                    to: '/profile'
+                },
+                {
+                    label: 'Logout',
+                    icon: 'fa-solid fa-right-to-bracket',
+                    command: () => {
+                        this.$auth0.logout({ returnTo: window.location.origin })
+                    }
+                }
+            ]
+        }
+    },
+    methods: {
+        login() {
+            this.$auth0.loginWithRedirect()
+        },
+        toggleProfileMenu(event) {
+            this.$refs.profileMenu.toggle(event)
+        }
     }
 }
 </script>
+
+<style scoped>
+</style>
