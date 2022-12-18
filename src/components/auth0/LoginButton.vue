14bb2ab50b5e0686a37438cc1ab6d2e43d6d3f75
Thomas Nichols <ta.nichols@outlook.com>
2022-12-17 20:35:58 -0800

    auth0

---
+++ src/components/auth0/LoginButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <button @click="login">Log in</button>
+</template>
+  
+<script>
+export default {
+    methods: {
+        login() {
+            this.$auth0.loginWithRedirect();
+        }
+    }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
 import { createApp } from 'vue'
+import { createAuth0 } from '@auth0/auth0-vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
-
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(
+    createAuth0({
+        domain: "dev-tt5z4nesi4knte53.us.auth0.com",
+        client_id: "8JpnQUAIbx1YV5L0RfNqUVEUv6G8wvhj",
+        redirect_uri: "http://localhost:5000"
+    })
+)
 
 app.mount('#app')
